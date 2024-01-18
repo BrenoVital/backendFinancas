a45e1cf1bd8c1075318bfea238e8c539b5cf2259
Breno Vital <brenovital50@gmail.com>
2024-01-18 12:08:53 -0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { server } from "./server/server";
+import server from "./server/server";
 
 server.listen(process.env.PORT, () => {
   console.log(`App rodando na porta ${process.env.PORT}`);

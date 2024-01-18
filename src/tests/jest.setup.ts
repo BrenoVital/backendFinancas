47f8dbf9e1be2585f019921e488cd908c62699db
Breno Vital <brenovital50@gmail.com>
2024-01-18 12:10:43 -0300

    deploy

--- src/tests/jest.setup.ts
+++ src/tests/jest.setup.ts
@@ -1,6 +1,6 @@
 import supertest from "supertest";
-import { server } from "../server/server";
 import { Knex } from "../server/database/knex/types";
+import server from "../server/server";
 
 beforeAll(async () => {
   await Knex.migrate.latest();

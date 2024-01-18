42f527c9d85b23669a11822a5e87d4a812515011
Breno Vital <brenovital50@gmail.com>
2024-01-18 12:06:21 -0300

    deploy tst

--- build/server/database/ETablesNames.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ETablesNames = void 0;
-var ETablesNames;
-(function (ETablesNames) {
-    ETablesNames["despesa"] = "despesa";
-})(ETablesNames || (exports.ETablesNames = ETablesNames = {}));

3f9c78559963274340b08518ff933877d3dbb118
Breno Vital <brenovital50@gmail.com>
2024-01-19 15:56:27 -0300

    feat: teste query com chave estrangeira

--- src/server/database/providers/despesas/GetAll.ts
+++ src/server/database/providers/despesas/GetAll.ts
@@ -9,11 +9,19 @@ export const getAll = async (
 ): Promise<IDespesa[] | Error | undefined> => {
   try {
     const result = await Knex(ETablesNames.despesa)
-      .select("*")
+      .join(
+        ETablesNames.categoria,
+        `${ETablesNames.despesa}.categoria_id`,
+        "=",
+        `${ETablesNames.categoria}.id`
+      )
+      .select(
+        `${ETablesNames.despesa}.*`,
+        `${ETablesNames.categoria}.nomeCategoria as categoria`
+      )
       .orWhere("descricao", "like", `%${filter}%`)
       .offset(page * take)
       .limit(take);
-
     return result;
   } catch (error) {
     console.log(error);

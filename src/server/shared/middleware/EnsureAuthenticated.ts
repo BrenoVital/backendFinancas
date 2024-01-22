e7a51587e21706a778194f4ff31b32b82eac1cfb
Breno Vital <brenovital50@gmail.com>
2024-01-22 14:44:18 -0300

    feat: finalizando a criação da autenticação por token

--- src/server/shared/middleware/EnsureAuthenticated.ts
+++ src/server/shared/middleware/EnsureAuthenticated.ts
@@ -1,5 +1,6 @@
 import { RequestHandler } from "express";
 import { StatusCodes } from "http-status-codes";
+import { JWTService } from "../services";
 
 export const ensureAuthenticated: RequestHandler = (req, res, next) => {
   const { authorization } = req.headers;
@@ -18,11 +19,20 @@ export const ensureAuthenticated: RequestHandler = (req, res, next) => {
     });
   }
 
-  if (!token) {
-    return res.status(StatusCodes.UNAUTHORIZED).json({
-      errors: { default: "Não autenticado." },
+  const jwtData = JWTService.verify(token);
+
+  if (jwtData === "JWT_SECRET_NOT_FOUND") {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      errors: { default: "Erro ao verificar o token" },
     });
+  } else if (jwtData === "INVALID_TOKEN") {
+    if (jwtData === "INVALID_TOKEN") {
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        errors: { default: "Não autenticado" },
+      });
+    }
   }
 
+  req.headers.idUsuario = jwtData.uid.toString();
   return next();
 };

7d9c030b6a02dc4783c5a453f05b72c8646646bf
Breno Vital <brenovital50@gmail.com>
2024-01-22 15:06:01 -0300

    feat: cors

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,10 +2,18 @@ import express from "express";
 import { router } from "./routes";
 import "dotenv/config";
 import "./shared/services/TraducoesYup";
+import cors from "cors";
 
 const server = express();
 
+server.use(
+  cors({
+    origin: process.env.ENABLED_CORS?.split(",") || [],
+  })
+);
+
 server.use(express.json());
+
 server.use(router);
 
 export default server;

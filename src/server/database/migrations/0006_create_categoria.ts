584bf2db0e5d45a7d3b0a80c31b0df04cb78281d
Breno Vital <brenovital50@gmail.com>
2024-01-23 15:28:26 -0300

    fix

--- src/server/database/migrations/0006_create_categoria.ts
+++ src/server/database/migrations/0006_create_categoria.ts
@@ -2,15 +2,15 @@ import { Knex } from "knex";
 import { ETablesNames } from "../ETablesNames";
 
 export async function up(knex: Knex) {
-  return knex.schema
-    .createTable(ETablesNames.categoria, (table) => {
-      table.bigIncrements("id").primary().index();
-      table.string("nomeCategoria").index().notNullable();
-      table.comment("Tabela de categorias");
-    })
-    .then(() => {
-      console.log(`Tabela categoria criada com sucesso.`);
-    });
+  return knex.schema;
+  // .createTable(ETablesNames.categoria, (table) => {
+  //   table.bigIncrements("id").primary().index();
+  //   table.string("nomeCategoria").index().notNullable();
+  //   table.comment("Tabela de categorias");
+  // })
+  // .then(() => {
+  //   console.log(`Tabela categoria criada com sucesso.`);
+  // });
 }
 
 export async function down(knex: Knex) {

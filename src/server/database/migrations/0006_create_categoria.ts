31cf9cedd9fbd184ab77145bf9c98562b84d3790
Breno Vital <brenovital50@gmail.com>
2024-01-23 14:05:28 -0300

    fix: rebuild

--- src/server/database/migrations/0006_create_categoria.ts
+++ src/server/database/migrations/0006_create_categoria.ts
@@ -1,20 +1,20 @@
-import { Knex } from "knex";
-import { ETablesNames } from "../ETablesNames";
+// import { Knex } from "knex";
+// import { ETablesNames } from "../ETablesNames";
 
-export async function up(knex: Knex) {
-  return knex.schema
-    .createTable(ETablesNames.categoria, (table) => {
-      table.bigIncrements("id").primary().index();
-      table.string("nomeCategoria").index().notNullable();
-      table.comment("Tabela de categorias");
-    })
-    .then(() => {
-      console.log(`Tabela categoria criada com sucesso.`);
-    });
-}
+// export async function up(knex: Knex) {
+//   return knex.schema
+//     .createTable(ETablesNames.categoria, (table) => {
+//       table.bigIncrements("id").primary().index();
+//       table.string("nomeCategoria").index().notNullable();
+//       table.comment("Tabela de categorias");
+//     })
+//     .then(() => {
+//       console.log(`Tabela categoria criada com sucesso.`);
+//     });
+// }
 
-export async function down(knex: Knex) {
-  return knex.schema.dropTable(ETablesNames.categoria).then(() => {
-    console.log(`Tabela ${ETablesNames.categoria} excluída com sucesso.`);
-  });
-}
+// export async function down(knex: Knex) {
+//   return knex.schema.dropTable(ETablesNames.categoria).then(() => {
+//     console.log(`Tabela ${ETablesNames.categoria} excluída com sucesso.`);
+//   });
+// }

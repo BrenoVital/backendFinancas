802d4c6108c793243dde4a3e1b490c9c25cc74a4
Breno Vital <brenovital50@gmail.com>
2024-01-23 11:09:08 -0300

    fix

---
+++ src/server/database/migrations/0005_create_usuario.ts
@@ -0,0 +1,22 @@
+import { Knex } from "knex";
+import { ETablesNames } from "../ETablesNames";
+
+export async function up(knex: Knex) {
+  return knex.schema
+    .createTable(ETablesNames.usuario, (table) => {
+      table.bigIncrements("id").primary().index();
+      table.string("nome").notNullable().checkLength(">=", 3);
+      table.string("email").unique().notNullable().checkLength(">=", 5);
+      table.string("senha").index().notNullable().checkLength(">=", 6);
+      table.comment("Tabela de usuários do sistema.");
+    })
+    .then(() => {
+      console.log(`Tabela ${ETablesNames.usuario} criada com sucesso.`);
+    });
+}
+
+export async function down(knex: Knex) {
+  return knex.schema.dropTable(ETablesNames.categoria).then(() => {
+    console.log(`Tabela ${ETablesNames.usuario} excluída com sucesso.`);
+  });
+}

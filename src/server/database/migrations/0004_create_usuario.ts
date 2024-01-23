d410c94ce4db59b3d49576ba6a6b58515b7556fd
Breno Vital <brenovital50@gmail.com>
2024-01-23 14:04:34 -0300

    fix: tst

--- src/server/database/migrations/0004_create_usuario.ts
+++ src/server/database/migrations/0004_create_usuario.ts
@@ -2,17 +2,21 @@ import { Knex } from "knex";
 import { ETablesNames } from "../ETablesNames";
 
 export async function up(knex: Knex) {
-  return knex.schema
-    .createTable(ETablesNames.usuario, (table) => {
-      table.bigIncrements("id").primary().index();
-      table.string("nome").notNullable().checkLength(">=", 3);
-      table.string("email").unique().notNullable().checkLength(">=", 5);
-      table.string("senha").index().notNullable().checkLength(">=", 6);
-      table.comment("Tabela de usuários do sistema.");
-    })
-    .then(() => {
-      console.log(`Tabela ${ETablesNames.usuario} criada com sucesso.`);
-    });
+  return knex.schema.hasTable(ETablesNames.usuario).then((exists) => {
+    if (!exists) {
+      return knex.schema
+        .createTable(ETablesNames.usuario, (table) => {
+          table.bigIncrements("id").primary().index();
+          table.string("nome").notNullable().checkLength(">=", 3);
+          table.string("email").unique().notNullable().checkLength(">=", 5);
+          table.string("senha").index().notNullable().checkLength(">=", 6);
+          table.comment("Tabela de usuários do sistema.");
+        })
+        .then(() => {
+          console.log(`Tabela ${ETablesNames.usuario} criada com sucesso.`);
+        });
+    }
+  });
 }
 
 export async function down(knex: Knex) {
